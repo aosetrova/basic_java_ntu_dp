3a999e66d73545c5c997378ff11b98e50ffa2cd1
Anhelina Osetrova <angel13osetrova@gmail.com>
2025-03-02 14:59:50 +0200

    Hello world app

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Lab work 0
+ * Created by Anhelina Osetrova
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
